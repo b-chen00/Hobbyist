545af0db8da45063bfce6588fa3cf53df3c01b30
Brandon Chen <bc2982@nyu.edu>
2022-11-08 02:04:16 -0500

    removed duplicate src/ in view path

--- src/app.mjs
+++ src/app.mjs
@@ -18,7 +18,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.set('view engine', 'hbs');
-app.set('views', `${__dirname}/src/views`);
+app.set('views', `${__dirname}/views`);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
