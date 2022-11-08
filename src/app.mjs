1ed1e4d5c24af996f5cc23affc5584b3887e3c21
Brandon Chen <bc2982@nyu.edu>
2022-11-08 02:03:27 -0500

    another attempt at fixing views

--- src/app.mjs
+++ src/app.mjs
@@ -18,8 +18,8 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.set('view engine', 'hbs');
-
-app.use(express.static(path.join(__dirname, '/public')));
+app.set('views', `${__dirname}/src/views`);
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
     secret: 'feline good',
